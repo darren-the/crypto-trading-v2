1050b6e7d4bf168470c2038330aca3f791d62289
darren <dtrainn7@gmail.com>
2023-01-26 18:58:56 +1100

    fix CORS

--- api/app.py
+++ api/app.py
@@ -2,9 +2,11 @@ from flask import Flask, request
 from google.cloud import bigquery
 import pandas as pd
 import decimal
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/candles', methods=['GET'])
 def candles():
