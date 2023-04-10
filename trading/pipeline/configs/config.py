43cd7232ce4e9e141125501b3c3e306b0cc70251
darren <dtrainn7@gmail.com>
2023-04-10 21:14:46 +1000

    fix highlow transformer to update each minute

--- trading/pipeline/configs/config.py
+++ trading/pipeline/configs/config.py
@@ -1,16 +1,16 @@
 local_env_data = './data'
 symbols = ['BTCUSD']
 timeframes = [
-    # '1m',
+    '1m',
     '5m',
-    # '15m',
+    '15m',
     # '30m',
-    # '1h',
+    '1h',
     # '2h',
-    # '4h',
+    '4h',
     # '6h',
     # '12h',
-    # '1D',
+    '1D',
     # '2D',
 ]
 base_timeframe = '1m'
--- trading/pipeline/steps/transformers/high_low.py
+++ trading/pipeline/steps/transformers/high_low.py
@@ -6,37 +6,47 @@ from copy import deepcopy
 
 class HighLow(Task):
     def __init__(self, *args, **kwargs):
+        self.pivot = 5
         self.__dict__.update(kwargs)
         self.current_candles = []
-        self.high_low = {
-            'timestamp': -1,
-            'candle_timestamp': -1,
-            'is_high': False,
-            'high_timestamp': -1,
-            'high_top': -1,
-            'high_bottom': -1,
-            'is_low': False,
-            'low_timestamp': -1,
-            'low_top': -1,
-            'low_bottom': -1,
-            'is_complete': False,
-        }
+        self.high_timestamp = -1
+        self.high_top = -1
+        self.high_bottom = -1
+        self.low_timestamp = -1
+        self.low_top = -1
+        self.low_bottom = -1
         self.alpha = 0
+        self.high_low = {}
         super().__init__()
 
     def process(self, element):
-        self.high_low['timestamp'] = element['timestamp']
-        self.high_low['candle_timestamp'] = element['candle_timestamp']
-        self.high_low['is_complete'] = element['is_complete']
-        if self.high_low['is_high']:
-            self._reset_high()
-        if self.high_low['is_low']:
-            self._reset_low()
-            
-        if element['is_complete'] is True:
+        self.high_low = {
+            'timestamp': element['timestamp'],
+            'candle_timestamp': element['candle_timestamp'],
+            'is_high': False,
+            'high_timestamp': self.high_timestamp,
+            'high_top': self.high_top,
+            'high_bottom': self.high_bottom,
+            'is_low': False,
+            'low_timestamp': self.low_timestamp,
+            'low_top': self.low_top,
+            'low_bottom': self.low_bottom,
+            'is_complete': element['is_complete'],
+        }
+        
+        # update current candles with new element
+        if len(self.current_candles) == 0:
             self.current_candles.append(element)
-            
-            if len(self.current_candles) == self.pivot:  # 5
+        else:
+            if self.current_candles[-1]['candle_timestamp'] == element['candle_timestamp']:
+                self.current_candles[-1] = element
+            else:
+                self.current_candles.append(element)
+
+        # update highs, lows and alpha
+        if len(self.current_candles) >= self.pivot:
+            if self.alpha == 0 and element['is_complete']:
+                # initialise highs and lows
                 self.alpha = self._calculate_alpha()
 
                 if self.alpha > 0:  # currently searching for a high
@@ -49,25 +59,31 @@ class HighLow(Task):
                     for candle in self.current_candles[1:]:
                         self._update_low(candle)
 
-            elif len(self.current_candles) > self.pivot:
-                self.current_candles.pop(0)  # Maintain current candles window at the pivot length
-                self._update_high(element) if self.alpha > 0 else self._update_low(element)  # update current direction
+            if len(self.current_candles) > self.pivot:
+                self.current_candles.pop(0)
+
+            self._update_high(element) if self.alpha > 0 else self._update_low(element)
+
+            if element['is_complete']:
+                self._save_high_low()
 
                 # check for direction change
                 new_alpha = self._calculate_alpha()
                 if new_alpha != self.alpha:  # a substantial direction change has occured
                     if new_alpha > 0:
                         self.high_low['is_low'] = True
+                        self._reset_low()
                     else:
                         self.high_low['is_high'] = True
+                        self._reset_high()
                     self.alpha = new_alpha
+
+        return self.high_low
         
-        return deepcopy(self.high_low)
-    
     def _calculate_alpha(self):
         base_alpha = spearmanr(np.arange(self.pivot), [candle['close'] for candle in self.current_candles])[0]
         return 1 if base_alpha >= 0 else -1
-    
+
     def _update_high(self, element, update_low=True):
         # Update high
         bottom = max(element['open'], element['close'])
@@ -77,8 +93,7 @@ class HighLow(Task):
             self.high_low['high_top'] = element['high']
         if bottom > self.high_low['high_bottom']:
             self.high_low['high_bottom'] = bottom
-        
-        
+
         # Also update the low
         if update_low:
             if self.high_low['low_timestamp'] != -1:
@@ -88,7 +103,7 @@ class HighLow(Task):
                     self._update_low(element, False)
             if self.high_low['low_timestamp'] == -1 and element['open'] > element['close']:
                 self._init_low(element)
-
+    
     def _update_low(self, element, update_high=True):
         # Update low
         top = min(element['open'], element['close'])
@@ -99,7 +114,7 @@ class HighLow(Task):
         if top < self.high_low['low_top']:
             self.high_low['low_top'] = top
         
-
+        # Also update the high
         if update_high:
             if self.high_low['high_timestamp'] != -1:
                 if self.high_low['low_timestamp'] > self.high_low['high_timestamp']:
@@ -120,9 +135,15 @@ class HighLow(Task):
         self.high_low['low_bottom'] = element['low']
     
     def _reset_high(self):
-        self.high_low['is_high'] = False
-        self.high_low['high_timestamp'] = self.high_low['high_top'] = self.high_low['high_bottom'] = -1
+        self.high_timestamp = self.high_top = self.high_bottom = -1
     
     def _reset_low(self):
-        self.high_low['is_low'] = False
-        self.high_low['low_timestamp'] = self.high_low['low_top'] = self.high_low['low_bottom'] = -1
+        self.low_timestamp = self.low_top = self.low_bottom = -1
+    
+    def _save_high_low(self):
+        self.high_timestamp = self.high_low['high_timestamp']
+        self.high_top = self.high_low['high_top']
+        self.high_bottom = self.high_low['high_bottom']
+        self.low_timestamp = self.high_low['low_timestamp']
+        self.low_top = self.high_low['low_top']
+        self.low_bottom = self.high_low['low_bottom']
